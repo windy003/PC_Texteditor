42fb4501bec42a7ee641e05e910964100fcac9cc
unknown <928920471@qq.com>
2026-01-26 12:51:11 +0800

    调整

--- run_editor.bat
+++ run_editor.bat
@@ -1,3 +1,3 @@
 @echo off
-cd /d "D:\files\using\PC_Texteditor"
-start /B pythonw "D:\files\using\PC_Texteditor\1.py"
+cd /d "D:\files\using\Python\PC_Texteditor"
+start /B pythonw "D:\files\using\Python\PC_Texteditor\1.py"
